bd516f3166f6d5ee7e240379bd60e895d3d3880c
Asadbek <braverlrgit@gmail.com>
2024-01-15 21:43:42 +0500

    Made StudentRepo singleton, added upload and load functions to db

--- src/uz/pdp/db/StudentRepo.java
+++ src/uz/pdp/db/StudentRepo.java
@@ -1,14 +1,49 @@
 package uz.pdp.db;
 
 import uz.pdp.entity.Student;
-
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class StudentRepo implements Repository<Student> {
+    private List<Student> students;
+    private static StudentRepo singleton;
+    private static String PATH = "src/uz/pdp/db/student_db.txt";
 
+    private StudentRepo(List<Student> students) {
+        this.students = students;
+    }
+    public static StudentRepo getInstance(){
+        if(singleton == null){
+            singleton = new StudentRepo(loadData());
+        }
+        return singleton;
+    }
+
+    private static ArrayList<Student> loadData(){
+        try (
+                InputStream inputStream = new FileInputStream(PATH);
+                ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+        ) {
+            return (ArrayList<Student>) objectInputStream.readObject();
+        } catch (Throwable e) {
+            return new ArrayList<>();
+        }
+    }
     @Override
     public void add(Student student) {
-
+        students.add(student);
+    }
+    private void upload(){
+        try (
+                OutputStream outputStream = new FileOutputStream(PATH);
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream)
+        ){
+            objectOutputStream.writeObject(students);
+        }
+        catch (IOException e){
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
